c23490eb9518ea94e1b18af4ba6534e829680ddf
Jake Real <jakereal2004@gmail.com>
2024-01-10 10:17:16 +0000

    Create basic sound class

---
+++ Sound.cs
@@ -0,0 +1,13 @@
+﻿using System.Media;
+
+namespace USWGame
+{
+    internal class Sound
+    {
+        public static void PlaySound(Stream audio)
+        {
+            using SoundPlayer player = new SoundPlayer(audio);
+            player.Play();
+        }
+    }
+}
